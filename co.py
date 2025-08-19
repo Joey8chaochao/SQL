0887141650dbac5eb7fea2c0765d626339121a45
joeywang <chaochao1988522@gmail.com>
2025-08-19 21:03:34 +1200

    conflicts

--- co.py
+++ co.py
@@ -1,7 +1,2 @@
 rint('welcome to the tip calculator!')
 
-total_amount = (total_bill + tips)
-each_people = (total_amount / people)
-# print(f"Each person should pay: ${each_people:.2f}")
-6 = 100 
-
