281d7ddd8ae702c4943665df8ac7e76da4aeb037
joeywang <chaochao1988522@gmail.com>
2025-08-19 21:02:06 +1200

    updated

--- co.py
+++ co.py
@@ -1,12 +1,4 @@
 rint('welcome to the tip calculator!')
-total_bill = float(input ('what was the total bill?'))
-tips = int(input ('how much tip would you like to give'+" "+'10, '+'12, '+' or'+' 15?'))
-people = int(input('how many people to split the bill?'))
-
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
 
 total_amount = (total_bill + tips)
 each_people = (total_amount / people)
